edfff46505866cc5de4cc96783bef6bd33e481e2
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-08-02 02:35:04 +0000

    bot: Edit social command.

--- pythonrobot_discord_bot/commands/social-commands.py
+++ pythonrobot_discord_bot/commands/social-commands.py
@@ -7,9 +7,19 @@ class SocialCommandsCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(description="Help Command")
-    async def help(self, interaction: discord.Interaction) -> None:
-        embed = discord.Embed(title="Help", description="This is a help command", color=0x00FF00)
+    @app_commands.command(description="Lists social links.")
+    async def socials(self, interaction: discord.Interaction) -> None:
+        embed = discord.Embed(title="Socials", description="", color=discord.Colour.red())
+        embed.add_field(name="GitHub", value="https://github.com/adambirds", inline=False)
+        embed.add_field(
+            name="Twitch", value="https://twitch.tv/codingandgamingwithadam", inline=False
+        )
+        embed.add_field(
+            name="YouTube",
+            value="https://www.youtube.com/channel/UCOJ37qE6yr-7fspsgzpTGkQ",
+            inline=False,
+        )
+        embed.add_field(name="Instagram", value="https://instagram.com/adambirds", inline=False)
         await interaction.response.send_message(embed=embed)
 
 
